f84eb854b3d5ce6b9ace8e93381aaa12560c338b
Vaasu02 <vasuchourasia625@gmail.com>
2025-04-17 11:36:08 +0530

    added-project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,43 +11,53 @@ import p2image from "../../public/images/projects/foodordering.png";
 import p3image from "../../public/images/projects/zoom.png";
 import p4image from "../../public/images/projects/ai.png";
 import p5image from "../../public/images/projects/blog.png";
+import p6image from "../../public/images/projects/chat-application.png";
 
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 import TransitionEffect from "@/components/TransitionEffect";
 
-const FramerImage=motion(Image);
+const FramerImage = motion(Image);
 
 const FeaturedProjects = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light
+    <article
+      className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light
     lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
-    ">
-        <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]'/>
+    "
+    >
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
       <Link
         href={link}
         target="_blank"
         className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
       >
-        <FramerImage src={img} alt="notfound" className="w-full h-auto" 
-        whileHover={{scale:1.05}}
-        transition={{duration:0.2}}
-        priority
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
-
-
+        <FramerImage
+          src={img}
+          alt="notfound"
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
 
       <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">{type}</span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">
+          {type}
+        </span>
         <Link
           href={link}
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">{title}</h2>
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">
+            {title}
+          </h2>
         </Link>
-        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">{summary}</p>
+        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">
+          {summary}
+        </p>
         <div className="mt-2 flex items-center">
           <Link href={github} target="_blank" className="w-10">
             {" "}
@@ -69,31 +79,37 @@ const FeaturedProjects = ({ type, title, summary, img, link, github }) => {
 const Project = ({ title, type, img, link, github }) => {
   return (
     <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4">
-         <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light md:-rigth-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]'/>
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light md:-rigth-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
       <Link
         href={link}
         target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-lg"
       >
-        <FramerImage src={img} alt="notfound" className="w-full h-auto" 
-        whileHover={{scale:1.05}}
-        transition={{duration:0.2}}
-        
+        <FramerImage
+          src={img}
+          alt="notfound"
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
         />
       </Link>
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">{type}</span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">
+          {type}
+        </span>
         <div
-          // href={link} //yaha Link ko div banaya hu
-          // target="_blank"
-          // className="hover:underline underline-offset-2"
+        // href={link} //yaha Link ko div banaya hu
+        // target="_blank"
+        // className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
+            {title}
+          </h2>
         </div>
 
         <div className="w-full mt-2 flex items-center justify-between">
-        <Link
+          <Link
             href={link}
             target="_blank"
             className="text-lg font-semibold underline md:text-base"
@@ -103,7 +119,6 @@ const Project = ({ title, type, img, link, github }) => {
           <Link href={github} target="_blank" className="w-8 md:w-6">
             <GithubIcon />{" "}
           </Link>
-          
         </div>
       </div>
     </article>
@@ -117,7 +132,7 @@ const projects = () => {
         <title>Portfolio | Project Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <TransitionEffect/>
+      <TransitionEffect />
       <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
@@ -137,6 +152,20 @@ const projects = () => {
                 type="Featured Project"
               />
             </div>
+            <div className="col-span-12">
+              <FeaturedProjects
+                title="Chat Application"
+                img={p6image}
+                summary="A real-time Chat Application built with MERN, Tailwind CSS, DaisyUI, and Socket.io.
+It features secure JWT-based Authentication & Authorization, online user tracking, and live messaging.
+Zustand is used for global state management, with robust error handling on both client and server sides.
+
+"
+                link="https://chat-app-q3qq.onrender.com/login"
+                github="https://github.com/Vaasu02/Chat-Application"
+                type="Featured Project"
+              />
+            </div>
             <div className="col-span-6 sm:col-span-12">
               <Project
                 title="Zoom Clone"
@@ -148,7 +177,7 @@ const projects = () => {
               />
             </div>
             <div className="col-span-6 sm:col-span-12">
-            <Project
+              <Project
                 title="AI Chatterpillar"
                 img={p4image}
                 summary="ChatterPillar is an advanced AI-powered chat application developed using the GEMINI API and the MENR stack (MongoDB, Express.js, Node.js, and React). It features robust user authentication provided by Clerk, ensuring a secure and personalized chat experience."
@@ -158,7 +187,7 @@ const projects = () => {
               />
             </div>
             <div className="col-span-12">
-            <FeaturedProjects
+              <FeaturedProjects
                 title="BloggingHub"
                 img={p5image}
                 summary="A feature-rich Blog Website built using React.js, Tailwind CSS, MongoDB, and Mongoose. It allows users to register, create, and manage blog posts. The website features dynamic radial gradient backgrounds, responsive design, SEO-friendly metadata, and loading states for smooth user experience."
@@ -168,7 +197,7 @@ const projects = () => {
               />
             </div>
             <div className="col-span-6 sm:col-span-12">
-            <Project
+              <Project
                 title="Modern UI/UX landing Page"
                 img={p1image}
                 summary="A feature-rich Landing page using React, Tailwind CSS. 
@@ -179,7 +208,7 @@ const projects = () => {
               />
             </div>
             <div className="col-span-6 sm:col-span-12">
-            <Project
+              <Project
                 title="SaaS Project"
                 img={project1}
                 summary="Working...."
@@ -188,7 +217,6 @@ const projects = () => {
                 type="Featured Project"
               />
             </div>
-            
           </div>
         </Layout>
       </main>
